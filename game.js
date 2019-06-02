d642c0db2ff6c10f4083f676a10f25d5755a01ce
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2019-06-02 22:13:52 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
-var money = new Decimal(10)
-loopInfinity = -1
+import Math
+var money = 10
+var lastUpdate = Date.now()
 
 function getMoney() {
-  money = money.add(1)
+  money = money + 1
   document.getElementById(“clicks”).textContent = “Money : ” + money
 }
 
@@ -10,7 +11,7 @@ var gens = []
 
 for(let i = 0; i < 4; i++) {
   let aGen = {
-    cost: new Decimal(10).pow(3 * i * i)
+    cost: Math.pow(10, Math.pow(i, 2))
     bought: 0
     amount: 0
     mult: 1
@@ -19,7 +20,24 @@ for(let i = 0; i < 4; i++) {
 }
 
 function updateGUI() {
-  document.getElementById(
+  document.getElementById(“money”).textContent = “money = ” + money
   for(let i = 0; i < 4; i++) {
     let g = gens[i]
     document.getElementByID(“gen” + (i + 1)).innerHTML = “Amount : ” + g.amount + “<br bought: >” + g.bought + “<br mult: >” + g.mult + “x<br>cost: ” + g.cost
+
+function productionLoop(diff) {
+  money += gens[0].amount * gens[0].mult * diff
+  for(let i = 1; i < 4; i++) {
+    gens[i - 1].amount += gens[i].amount * gens[i].mult * diff
+  }
+}
+function mainLoop() {
+  var diff = Date.now() - lastUpdate / 1000
+  productionLoop(diff)
+  updateGUI()
+  lastUpdate = Date.now()
+}
+
+setInterval(mainLoop, 50)
+
+updateGUI()
