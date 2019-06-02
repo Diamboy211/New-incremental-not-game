5ec9d3ce7aa07d20a14b9dfc9eef501c355d64f5
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2019-06-02 21:30:41 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,19 @@
 var money = new Decimal(10)
+loopInfinity = -1
 
 function getMoney() {
   money = money.add(1)
   document.getElementById(“clicks”).textContent = “Money : ” + money
+}
+
+var gens = []
+
+for(let i = 0; i < 4; i++) {
+  let aGen = {
+    cost: new Decimal(10).pow(3 * i * i)
+    bought: 0
+    amount: 0
+    mult: 1
+  }
+  gens.push(aGen)
 }
