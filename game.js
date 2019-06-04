e2a49b85e267bc40521e72639b94b73e6d961b7c
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2019-06-04 13:17:15 +0700

    Update game.js

--- game.js
+++ game.js
@@ -11,22 +11,22 @@ var gens = []
 
 for(let i = 0; i < 4; i++) {
   let aGen = {
-    cost: Math.pow(10, Math.pow(i, 2))
-    bought: 0
-    amount: 0
+    cost: Math.pow(10, Math.pow(i, 2));
+    bought: 0;
+    amount: 0;
     mult: 1
   }
   gens.push(aGen)
 }
 
 function updateGUI() {
-  document.getElementById(“money”).textContent = “money = ” + money
+  document.getElementById(“money”).textContent = “money = ” + money;
   for(let i = 0; i < 4; i++) {
     let g = gens[i];
     document.getElementByID(“gen” + (i + 1)).innerHTML = “Amount : ” + g.amount + “<br bought: >” + g.bought + “<br mult: >” + g.mult + “x<br>cost: ” + g.cost
 
 function productionLoop(diff) {
-  money += gens[0].amount * gens[0].mult * diff
+  money += gens[0].amount * gens[0].mult * diff;
   for(let i = 1; i < 4; i++) {
     gens[i - 1].amount += gens[i].amount * gens[i].mult * diff
   }
